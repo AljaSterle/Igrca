c6cc480fb784110d25f484553f56beb53c97b828
Alja Sterle <alja.sterle@gmail.com>
2025-01-26 23:22:11 +0100

    added things

--- fire.h
+++ fire.h
@@ -4,22 +4,22 @@
 #include "game_object.h"
 #include "sprite_renderer.h"
 
-class Fire : public GameObject {
+class SideObject : public GameObject {
 public: 
 	const float flek = 2.0f;
 	bool nekoc;
 	float flekCounter;
-	Fire(glm::vec2, glm::vec2, Texture2D);
+	SideObject(glm::vec2, glm::vec2, Texture2D);
 	void Draw(SpriteRenderer& renderer);
 
     // Define the copy constructor
-    Fire(const Fire& other)
+    SideObject(const SideObject& other)
         : GameObject(other), flek(other.flek), nekoc(other.nekoc), flekCounter(other.flekCounter) {
         // Copy other members if necessary
     }
 
     // Define the copy assignment operator
-    Fire& operator=(const Fire& other) {
+    SideObject& operator=(const SideObject& other) {
         if (this != &other) {
             GameObject::operator=(other);
             nekoc = other.nekoc;
@@ -30,7 +30,7 @@ public:
     }
 
     // Define the equality operator
-    bool operator==(const Fire& other) const {
+    bool operator==(const SideObject& other) const {
         return this->Position == other.Position && this->Size == other.Size && this->Sprite.ID == other.Sprite.ID;
     }
 };
